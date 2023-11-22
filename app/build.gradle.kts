069db741c58180e972d91f4b0f1c30fb04754993
devbrianmedina <brian@dercide.com>
2023-11-22 17:07:56 -0600

    update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,4 +48,5 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
     implementation("androidx.navigation:navigation-fragment-ktx:2.7.5")
     implementation("androidx.navigation:navigation-ui-ktx:2.7.5")
+    implementation("androidx.cardview:cardview:1.0.0")
 }
